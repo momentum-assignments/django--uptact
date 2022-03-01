8d5f5a4819377259f8567429bd506b6d02e007f2
Amy Gori <amydgori@gmail.com>
2022-03-01 18:10:23 -0500

    Update dependencies

--- uptact/settings.py
+++ uptact/settings.py
@@ -10,9 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
 from pathlib import Path
-
 import environ
 
 env = environ.Env(
